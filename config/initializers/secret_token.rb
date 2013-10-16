6f622063cc61fc6d7dc4d327ac8326c0e009917c
AhmadKadhim <akadhim13@gmail.com>
2013-10-16 13:25:18 -0400

    Initial commit w/ Foundation installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SenseAppeal::Application.config.secret_key_base = '142622cf5886db3fc7bd54bb12ecfbd14e6b68c003cdb1c97c1ef0963f59b2f77aaf731e909a5fba8d1523e34390e305cd07d170b4f1d846fbadadcf0128b53c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SenseAppeal::Application.config.session_store :cookie_store, key: '_sense_appeal_session'
